cf885e437036ff753b70f40e205d93e12d97bd66
萨耶摩尔丶槿花 <wzj-dev@AvalonSpacedeMacBook-Pro.local>
2018-02-26 21:10:45 +0800

    提交代码

---
+++ actions/query/GetRandomQuestionByConditionAction.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wzj-dev
+ * Date: 18/2/26
+ * Time: 下午9:10
+ */
+
+namespace app\modules\actions\query;
+use yii\base\Action;
+
+class GetRandomQuestionByConditionAction extends Action
+{
+    public function run(){
+        return "asd";
+    }
+}
